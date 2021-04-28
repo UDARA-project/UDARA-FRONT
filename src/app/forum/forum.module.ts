64035f0598160f680a1042e288378af25f9c0e24
Valentin <jajora78@gmail.com>
2021-04-28 16:48:34 +0200

    On a tout cassé

--- src/app/forum/forum.module.ts
+++ src/app/forum/forum.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListeRubriqueComponent } from './liste-rubrique/liste-rubrique.component';
 import { AjoutRubriqueComponent } from './ajout-rubrique/ajout-rubrique.component';
-import { FilConversationRubriqueComponent } from './fil-conversation-rubrique/fil-conversation-rubrique.component';
 import { ListeFilsConversationsComponent } from './liste-fils-conversations/liste-fils-conversations.component';
 import { ContenuFilConversationComponent } from './contenu-fil-conversation/contenu-fil-conversation.component';
 import { AjoutFilConversationComponent } from './ajout-fil-conversation/ajout-fil-conversation.component';
@@ -13,7 +12,6 @@ import { AjoutFilConversationComponent } from './ajout-fil-conversation/ajout-fi
   declarations: [
     ListeRubriqueComponent,
     AjoutRubriqueComponent,
-    FilConversationRubriqueComponent,
     ListeFilsConversationsComponent,
     ContenuFilConversationComponent,
     AjoutFilConversationComponent

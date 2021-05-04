d310060eece3561ceb8f0369a569df511083294f
nicolasH <nicolas.hornuel@gmail.com>
2021-05-04 15:19:30 +0200

    accueil

--- src/app/frame/page-accueil/accueil-data/accueil-data.component.ts
+++ src/app/frame/page-accueil/accueil-data/accueil-data.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Commune } from 'src/app/models/commune.interface';
 import { IndicateurAir } from 'src/app/models/indicateurAir.interface';
 import { NiveauMeteo } from 'src/app/models/niveauMeteo.interface';
@@ -30,74 +30,104 @@ export class AccueilDataComponent implements OnInit {
   niveauMeteos: NiveauMeteo[];
   indicateurWithUnit: IndicateursWithUnit[] = [];
   meteoWithUnit: MeteosWithUnit[] = [];
-  commune: Commune;
+  infoRecensement: string;
+  infoGPS: string;
   meteoGlobale: string = "../../../../assets/images/soleil.svg";
   indicateurGlobale: string = "../../../../assets/images/emoji-3.svg";
   temperatureActuelle: number;
   evolutionPolluant: number;
   nomIndicateurs: string[] = ["co", "no", "no2", "o3", "so2", "pm2_5", "pm10", "nh3"];
   nomNiveaux: string[] = ["Température", "Nuage", "Vent", "Pluie"]
-  echelleTemps: string[] = ["JOURNALIERE", "HEBDOMADAIRE", "MENSUEL"];
+  echelleTemps: any;
+  enteteColonne: string[] = [];
 
   constructor(private communeService: CommuneService,
     private indicateurAirService: IndicateurAirService,
     private niveauMeteoService: NiveauMeteoService) { }
 
   ngOnInit(): void {
-    this.renderVisiteur()
+
+    this.echelleTemps = 'JOURNALIERE';
+    this.search();
   }
 
-  renderVisiteur() {
+  setEchelleTemps(echelleTemps: string) {
+    this.echelleTemps = echelleTemps;
+    this.nomCommune == undefined ? this.search() : this.searchByCommune();
+  }
+
+  search() {
     this.loadingSearch = true;
-    this.indicateurAirService.search(this.nomIndicateurs, this.echelleTemps[0]).subscribe(res => {
-      res.forEach(elt => {
-        let newValue = elt.valeurs.map(r => r+"%");
-        this.indicateurWithUnit.push({nom:elt.nom, valeurs:newValue});
-      });
-      this.niveauMeteoService.search(this.nomNiveaux, this.echelleTemps[0]).subscribe(res => {
-        res.forEach(elt => {
-          let newValue = [];
-          if (elt.nom == "Température") {
-            newValue = elt.valeurs.map(r => r+"°C");
-          } else if (elt.nom == "Nuage" || elt.nom == "Pluie") {
-            newValue = elt.valeurs.map(r => r+"%");
-          } else if (elt.nom == "Vent") {
-            newValue = elt.valeurs.map(r => r+"m/s");
-          }
-          this.meteoWithUnit.push({nom:elt.nom, valeurs:newValue});
-        });
-
-        console.log(this.niveauMeteos);
-
-        this.commune = {
-          id: null,
-          name: null,
-          lat: null,
-          lon: null,
-          departement: null,
-          region: null,
-          population: null
-        }
-        this.loadingSearch = false;
+    this.indicateurAirs = [];
+    this.niveauMeteos = [];
+    this.indicateurAirService.search(this.nomIndicateurs, this.echelleTemps).subscribe(res => {
+      this.renderIndicateur(res);
+      this.niveauMeteoService.search(this.nomNiveaux, this.echelleTemps).subscribe(res => {
+        this.renderNiveau(res);
       })
     })
   }
 
-  renderSearchInsideParent(data) {
+  renderSearchInsideParent(nomCommune: string) {
+    this.nomCommune = nomCommune;
+    this.searchByCommune();
+  }
+
+  searchByCommune() {
     this.loadingSearch = true;
-    this.nomCommune = data.nomCommune;
-    this.indicateurAirs = data.indicateurAirs;
-    this.niveauMeteos = data.niveauMeteos;
-    this.communeService.findByName(data.nomCommune).subscribe(r => {
-      this.commune = r;
-      this.generateMeteoGlobale();
-      this.generateIndicateurGlobale();
-      this.loadingSearch = false;
+    this.indicateurAirs = [];
+    this.niveauMeteos = [];
+    this.indicateurAirService.searchByCommune(this.nomCommune, this.nomIndicateurs, this.echelleTemps).subscribe(res => {
+      this.renderIndicateur(res);
+      this.niveauMeteoService.searchByCommune(this.nomCommune, this.nomNiveaux, this.echelleTemps).subscribe(res => {
+        this.renderNiveau(res);
+      })
     })
   }
 
+  renderIndicateur(res) {
+    this.indicateurWithUnit = [];
+    this.indicateurAirs = res;
+    res.forEach(elt => {
+      let newValue = elt.valeurs.map(r => r + "%");
+      this.indicateurWithUnit.push({ nom: elt.nom, valeurs: newValue });
+    });
+    this.generateIndicateurGlobale();
+  }
+
+  renderNiveau(res) {
+    this.meteoWithUnit = [];
+    this.niveauMeteos = res;
+    res.forEach(elt => {
+      let newValue = [];
+      if (elt.nom == "Température") {
+        newValue = elt.valeurs.map(r => r + "°C");
+      } else if (elt.nom == "Nuage" || elt.nom == "Pluie") {
+        newValue = elt.valeurs.map(r => r + "%");
+      } else if (elt.nom == "Vent") {
+        newValue = elt.valeurs.map(r => r + "m/s");
+      }
+      this.meteoWithUnit.push({ nom: elt.nom, valeurs: newValue });
+    });
+    this.renderEnteteColonne();
+    this.generateMeteoGlobale();
+    this.renderRecensement();
+    this.loadingSearch = false;
+  }
+
+  renderRecensement() {
+    if (this.nomCommune == undefined) {
+      this.infoRecensement = "Données Globales Française"
+    } else {
+      this.communeService.findByName(this.nomCommune).subscribe(r => {
+        this.infoRecensement = `Commune : ${r.name}, ${r.population} Habitants (Région:${r.region})`;
+        this.infoGPS = `Coordonnées GPS de la station (latitude, longitude): ${r.lat}, ${r.lon}`;
+      })
+    }
+  }
+
   generateMeteoGlobale() {
-   /*  let nombreValeur = this.niveauMeteos[0].valeurs.length;
+    let nombreValeur = this.niveauMeteos[0].valeurs.length;
     this.temperatureActuelle = Math.round(this.niveauMeteos[0].valeurs[nombreValeur - 1]);
     let nuageValeur = this.niveauMeteos[1].valeurs[nombreValeur - 1];
     let ventValeur = this.niveauMeteos[2].valeurs[nombreValeur - 1];
@@ -112,18 +142,18 @@ export class AccueilDataComponent implements OnInit {
       this.meteoGlobale = "../../../../assets/images/nuage+vent.svg"
     } else if (nuageValeur > 66 && ventValeur > 10 && pluieValeur > 66) {
       this.meteoGlobale = "../../../../assets/images/nuage+vent+pluie.svg"
-    } */
+    }
   }
 
   generateIndicateurGlobale() {
-/*     let nombreValeur = this.indicateurAirs[0].valeurs.length;
+    let nombreValeur = this.indicateurAirs[0].valeurs.length;
     let cumulJour: number = 0;
     let cumulHier: number = 0;
     for (const indicateur of this.indicateurAirs) {
       cumulJour += indicateur.valeurs[nombreValeur - 1];
       cumulHier += indicateur.valeurs[nombreValeur - 2]
     }
-    this.evolutionPolluant = Math.round(cumulHier / (cumulHier - cumulJour));
+    this.evolutionPolluant = Math.round(((cumulJour - cumulHier) / cumulHier) * 100);
     if (this.evolutionPolluant > 25) {
       this.indicateurGlobale = "../../../../assets/images/emoji-1.svg"
     } else if (this.evolutionPolluant > 10) {
@@ -132,7 +162,63 @@ export class AccueilDataComponent implements OnInit {
       this.indicateurGlobale = "../../../../assets/images/emoji-5.svg"
     } else if (this.evolutionPolluant < 10) {
       this.indicateurGlobale = "../../../../assets/images/emoji-4.svg"
-    } */
+    }
   }
+
+  renderEnteteColonne() {
+    this.enteteColonne = [];
+    let nombreValeur = this.niveauMeteos[0].valeurs.length;
+    if (this.echelleTemps == "JOURNALIERE") {
+      let date = new Date();
+      let nbDuJour = date.getDay();
+      let nomDuJour;
+      for (let i = 0; i < nombreValeur; i++) {
+        nbDuJour-i<=0 ? nbDuJour = 7+i: nbDuJour;
+        switch (nbDuJour-i) {
+          case 1: nomDuJour = "lundi"; break;
+          case 2: nomDuJour = "mardi"; break;
+          case 3: nomDuJour = "mercredi"; break;
+          case 4: nomDuJour = "jeudi"; break;
+          case 5: nomDuJour = "vendredi"; break;
+          case 6: nomDuJour = "samedi"; break;
+          case 7: nomDuJour = "dimanche"; break;
+        }
+        this.enteteColonne.push(nomDuJour);
+      }
+    } else if (this.echelleTemps == "HEBDOMADAIRE") {
+      let date = new Date();
+      date.setHours(0, 0, 0, 0);
+      date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7); // Thursday in current week decides the year.
+      let week1 = new Date(date.getFullYear(), 0, 4); // January 4 is always in week 1.
+      let currentWeek = 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
+      for (let i = 0; i < nombreValeur; i++) {
+        this.enteteColonne.push(`semaine${currentWeek - i}`)
+      }
+    } else if (this.echelleTemps == "MENSUEL") {
+      let date = new Date();
+      let nbDuMois = date.getMonth()+1;
+      let nomDuMois;
+      for (let i = 0; i < nombreValeur; i++) {
+        nbDuMois-i<=0 ? nbDuMois = 12+i: nbDuMois;
+        switch (nbDuMois-i) {
+          case 1: nomDuMois = "janvier"; break;
+          case 2: nomDuMois = "février"; break;
+          case 3: nomDuMois = "mars"; break;
+          case 4: nomDuMois= "avril"; break;
+          case 5: nomDuMois = "mai"; break;
+          case 6: nomDuMois = "juin"; break;
+          case 7: nomDuMois = "juillet"; break;
+          case 8: nomDuMois = "aout"; break;
+          case 9: nomDuMois = "septembre"; break;
+          case 10: nomDuMois = "octobre"; break;
+          case 11: nomDuMois = "novembre"; break;
+          case 12: nomDuMois = "decembre"; break;
+        }
+        this.enteteColonne.push(nomDuMois);
+      }
+    }
+    this.enteteColonne.reverse();
+  }
+
 
 }
